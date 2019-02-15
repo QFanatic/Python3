d6a9c51e55535eb1634c8b50423ce5fe1fb41da6
QFanatic <qfanatic@gmail.com>
2019-02-15 11:57:03 -0800

    Add utils file

---
+++ utils.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+import math
+import sys
+
+def get_int(msg, minimum, default):
+    while True:
+        try:
+            line =input(msg)
+            if not line and default is not None:
+                return default
+            i = int(line)
+            if i < minimum:
+                print("must be >= ", minimum)
+            else:
+                return i
+        except ValueError as e:
+            print(e)
+
+def list_find(lst, target):
+    index = 0
+    while index < len(lst):
+        if lst[index] == target:
+            break
+        index += 1
+    else:
+        index = -1
+    return index
+
+def heron(a,b,c,*,units="meters"):
+    s = (a+b+c)/2
+    area = math.sqrt(s*(s-a)*(s-b)*(s-c))
+    return"{0} {1}".format(area,units)
+
+def get_string(message, name="string", default=None, minimum_length=0, maximum_length=80):
+    message += ": " if default is None else " [{0}]: ".format(default)
+    while True:
+        try:
+            line = input(message)
+            if not line:
+                if default is not None:
+                    return default
+                if minimum_length == 0:
+                    return ""
+                else:
+                    raise ValueError("{0} may not be empty".format(name))
+                if not(minimum_length <= len(line) <= maximum_length):
+                    raise ValueError("{0} must have at least {1} and "
+                    "at most {2} characters".format(name, minimum_length, maximum_length))
+                return line
+        except ValueError as err:
+            print("ERROR", err)
+
+def equal_float(a,b):
+    return math.abs(a-b) <= sys.float_info.epsilon
